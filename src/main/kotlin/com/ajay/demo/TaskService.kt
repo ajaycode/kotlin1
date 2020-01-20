1806b111d7bd51a735b29328199b2dfb494f58b3
Ajay <ajaycode>
2020-01-19 21:55:35 -0800

    Task

--- src/main/kotlin/com/ajay/demo/TaskService.kt
+++ src/main/kotlin/com/ajay/demo/TaskService.kt
@@ -1,4 +1,21 @@
-package com.ajay.demo.com.ajay.demo
+package com.ajay.demo
 
+import org.springframework.stereotype.Component
+
+@Component
 class TaskService {
+    fun findUser(num: Int): String {
+
+        var username: String
+        var message: String = "Welcome"
+        if (num == 7)
+            username = "Ajay"
+        else if (num == 5)
+            username = "Geethika"
+        else
+            username = "Arya"
+
+        return username
+
+    }
 }
