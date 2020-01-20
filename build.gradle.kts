0fa1988fb8cd29557db9fbe27a8b5783fc7b5fc8
Ajay <ajaycode>
2020-01-19 22:29:33 -0800

    Checkin entire directory. Added tasks for H2, JPA, Azure AD and Azure into Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	kotlin("jvm") version "1.3.61"
 	kotlin("plugin.spring") version "1.3.61"
+    kotlin("plugin.jpa") version "1.3.61"
 }
 
 group = "com.ajay"
@@ -15,16 +16,34 @@ repositories {
 	mavenCentral()
 }
 
+extra["azureVersion"] = "2.2.0"
+extra["springBootAdminVersion"] = "2.2.1"
+
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-	}
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("com.microsoft.azure:azure-active-directory-spring-boot-starter")
+    implementation("com.microsoft.azure:azure-spring-boot-starter")
+    implementation("de.codecentric:spring-boot-admin-starter-server")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:${springBootVersion}")
+    runtimeOnly("com.h2database:h2:1.3.176") runtimeOnly ("com.h2database:h2")
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
+
+dependencyManagement {
+    imports {
+        mavenBom("com.microsoft.azure:azure-spring-boot-bom:${property("azureVersion")}")
+        mavenBom("de.codecentric:spring-boot-admin-dependencies:${property("springBootAdminVersion")}")
+    }
+}
+
 
 tasks.withType<Test> {
 	useJUnitPlatform()
